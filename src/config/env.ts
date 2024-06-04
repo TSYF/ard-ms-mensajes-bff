83b5d0388545076327cd046ca623b107842ed17e
Tomás <tsyf1999@gmail.com>
2024-06-04 17:26:55 -0400

    HOST defined in env

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,7 @@ import { get } from 'env-var';
 
 export const envs = {
     PORT: get("PORT").required().asPortNumber(),
+    HOST: get("HOST").asString(),
     MESSAGE_ENDPOINT: get("MESSAGE_ENDPOINT").required().asUrlString(),
     DEFAULT_API_PREFIX: get("DEFAULT_API_PREFIX").required().asString(),
 }
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(morgan("combined"))
 app.use(express.json());
-const { PORT, DEFAULT_API_PREFIX } = envs;
+const { PORT, HOST, DEFAULT_API_PREFIX } = envs;
 
 app.use(DEFAULT_API_PREFIX, messageRouter);
-app.listen(PORT || 8000, () => console.log("MS-MENSAJES-BFF STARTED"));
+app.listen(PORT || 8000, HOST || "127.0.0.1", () => console.log("MS-MENSAJES-BFF STARTED"));
